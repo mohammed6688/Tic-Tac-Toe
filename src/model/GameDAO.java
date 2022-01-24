97bdcafd148c66d12511f15c786af47d25ed6535
RofidaReda <57877648+ROFIDA106@users.noreply.github.com>
2022-01-24 12:52:13 +0200

    features

--- src/model/GameDAO.java
+++ src/model/GameDAO.java
@@ -8,25 +8,25 @@ import java.util.logging.Logger;
 
 public class GameDAO {
 
-    public static final String DB_URL="jdbc:postgresql://localhost:5432/";
-    public static final String DB_NAME="java test";
-    public static final String USER="postgres";
-    public static final String PASS="123456";
+    public static final String DB_URL = "jdbc:postgresql://localhost:5432/";
+    public static final String DB_NAME = "java test";
+    public static final String USER = "postgres";
+    public static final String PASS = "123456";
 
     private Connection con;
     private Statement stmt;
 
     private void connect() throws SQLException {
-        con = DriverManager.getConnection(DB_URL+DB_NAME,USER,PASS);
-        stmt= con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY) ;
+        con = DriverManager.getConnection(DB_URL + DB_NAME, USER, PASS);
+        stmt = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
     }
 
-    public List <Player> getOnlinePlayers(){
+    public List<Player> getOnlinePlayers() {
         try {
-            String queryString= new String("select * from player where status = true");
-            ResultSet rs= stmt.executeQuery(queryString) ;
-            List <Player> onlinePlayers= new ArrayList<>();
-            while(rs.next()){
+            String queryString = new String("select * from player where status = true");
+            ResultSet rs = stmt.executeQuery(queryString);
+            List<Player> onlinePlayers = new ArrayList<>();
+            while (rs.next()) {
                 onlinePlayers.add(new Player(rs.getInt("id"),
                         rs.getString("username"),
                         rs.getString("email"),
@@ -35,56 +35,79 @@ public class GameDAO {
                         rs.getBoolean("inGame")));
             }
             return onlinePlayers;
-        }catch (SQLException ex) {
+        } catch (SQLException ex) {
             System.out.println("error in online players");
         }
 
         return null;
     }
 
-    public void getLeaderBoard(){
-
-    }
-
-    public void handleGameRequest(){
-
-    }
-
-    public void updatingGame(){
-
-    }
-
-    public String checkSignIn(String email, String password){
+    ///////////////////////////////////////////////////
+    public List<Player> getLeaderBoard() {
         try {
-            String queryString= new String("select * from player where email ="+email);
-            ResultSet rs= stmt.executeQuery(queryString);
-            Player player = null;
-            while(rs.next()){
-                player=new Player(rs.getInt("id"),
-                rs.getString("username"),
-                rs.getString("email"),
-                rs.getString("password"),
-                rs.getBoolean("status"),
-                rs.getBoolean("inGame"));
+            String queryString = new String("select id , username ,score from player ORDER BY score DESC ");
+            ResultSet rs = stmt.executeQuery(queryString);
+            List<Player> leaderBoard = new ArrayList<>();
+            while (rs.next()) {
+                leaderBoard.add(new Player(rs.getInt("id"),
+                        rs.getString("username"),
+                        rs.getString("score"),
+                        rs.getString("password"),
+                        rs.getBoolean("status"),
+                        rs.getBoolean("inGame")));
             }
+            return leaderBoard;
+        } catch (SQLException ex) {
+            System.out.println("error in online leaderboard");
+        }
 
-            if(player!=null && password.equals(player.password)){
-                return "Logged in successfully";
-            }else {
-                return "Password is not correct";
+        return null;
+
+    }
+    ///////////////////////////////////////////
+    /// update Score//////
+
+    public static void updateScore(int player_id, int score) {
+        try {
+            PreparedStatement preparedStatement = con.prepareStatement("UPDATE player SET score=? WHERE id=?");
+            preparedStatement.setInt(1, score);
+            preparedStatement.setInt(2, player_id);
+            int isUpdated = preparedStatement.executeUpdate();
+            if (isUpdated > 0) {
+                Player p = player.get(player_id);
+                p.setScore(score);
+                player.replace(player_id, p);
             }
         } catch (SQLException ex) {
-            return "Connection issue, please try again later";
+            Logger.getLogger(Player.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
-    public Player getEmailData(String email){
+    ////////////////////////////////////////////////////////
+
+    public void register() {
+
+    }
+
+    public void sendMessage() {
+
+    }
+
+    public void handleGameRequest() {
+
+    }
+
+    public void updatingGame() {
+
+    }
+
+    public String checkSignIn(String email, String password) {
         try {
-            String queryString= new String("select * from player where email ="+email);
-            ResultSet rs= stmt.executeQuery(queryString);
+            String queryString = new String("select * from player where email =" + email);
+            ResultSet rs = stmt.executeQuery(queryString);
             Player player = null;
-            while(rs.next()){
-                player=new Player(rs.getInt("id"),
+            while (rs.next()) {
+                player = new Player(rs.getInt("id"),
                         rs.getString("username"),
                         rs.getString("email"),
                         rs.getString("password"),
@@ -92,7 +115,31 @@ public class GameDAO {
                         rs.getBoolean("inGame"));
             }
 
-            if(player!=null){
+            if (player != null && password.equals(player.password)) {
+                return "Logged in successfully";
+            } else {
+                return "Password is not correct";
+            }
+        } catch (SQLException ex) {
+            return "Connection issue, please try again later";
+        }
+    }
+
+    public Player getEmailData(String email) {
+        try {
+            String queryString = new String("select * from player where email =" + email);
+            ResultSet rs = stmt.executeQuery(queryString);
+            Player player = null;
+            while (rs.next()) {
+                player = new Player(rs.getInt("id"),
+                        rs.getString("username"),
+                        rs.getString("email"),
+                        rs.getString("password"),
+                        rs.getBoolean("status"),
+                        rs.getBoolean("inGame"));
+            }
+
+            if (player != null) {
                 return player;
             }
         } catch (SQLException ex) {
