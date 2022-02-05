1b6c618dc0bbb4f3f817862f82f8b0b54a9b1d01
mohammed6688 <mohammed668800.ma@gmail.com>
2022-02-05 10:51:06 +0200

    fixing bugs

--- src/model/PlayerHandler.java
+++ src/model/PlayerHandler.java
@@ -191,7 +191,7 @@ public class PlayerHandler extends Thread {
                     player.isInGame());
 
         }
-        ps.println(" " + "null");
+        ps.println("null");
     }
 
     private void withdraw() throws SQLException {
@@ -283,8 +283,7 @@ public class PlayerHandler extends Thread {
         System.out.println(onlinePlayers.size());
         for (Player player : onlinePlayers) {
             if (!player.email.equals(email)) {         //to not send the current player as one of the online players
-                ps.println(onlinePlayers.size() + " " +
-                        player.id + " " +
+                ps.println(player.id + " " +
                         player.getUsername() + " " +
                         player.getEmail() + " " +
                         player.getPassword() + " " +       //TODO remove password from list
@@ -292,7 +291,7 @@ public class PlayerHandler extends Thread {
                         player.isInGame());
             }
         }
-        ps.println(" " + "null");
+        ps.println("null");
     }
 
     private void signUp() throws SQLException{
